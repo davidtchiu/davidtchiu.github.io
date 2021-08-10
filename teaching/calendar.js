26b4d9baab2b7610d1dc738d29f1ae6ec181e9bd
David Chiu <david.t.chiu@gmail.com>
2021-08-10 12:48:16 -0700

    calendar 240

--- teaching/calendar.js
+++ teaching/calendar.js
@@ -98,7 +98,7 @@ class CourseCalendar {
           // any assignments due today?
           for (let assign of this.days.assignments) {
             if (this.sameDay(new Date(assign.due), currentDate)) {
-              td.innerHTML += `<span style='color: #000080">${assign.name} due</span><br/><br/>`;
+              td.innerHTML += `<span style='color: #000080'>${assign.name} due</span><br/><br/>`;
             }
           }
 
