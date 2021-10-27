4f795446bf71cba3ed202ba1756eb7dec4c44d8c
David Chiu <david.t.chiu@gmail.com>
2021-10-26 22:30:43 -0700

    hwk 5 added to db

--- teaching/cs240/schedule.js
+++ teaching/cs240/schedule.js
@@ -29,7 +29,7 @@ let days = {
         name: "Hwk 5",
         title: "Simone the Memory Game",
         url: "hwk5.simone/",
-        due: "11/12/2021",
+        due: "11/15/2021",
       },
     ],
 
@@ -39,7 +39,7 @@ let days = {
         name: "Phase 1",
         title: "Planning and Design",
         url: "proj1.planning/",
-        due: "11/10/2021",
+        due: "11/12/2021",
       },
       // {
       //   name: "Phase 2.1",
@@ -176,7 +176,8 @@ let days = {
     "Persona and Scenarios Lab",
     // wk 11
     "User Stories and Features Lab",
-    "Templates: EJS",
+    "Work!",
+    // "Templates: EJS",
     // wk 12
     "Work/Sprint Review with David",
     "Software Testing; Testing lab",
