c7cfec122de86a0b498167e30f45259ebfcc6ead
David Chiu <david.t.chiu@gmail.com>
2023-09-25 22:59:44 -0700

    updates

--- teaching/cs455/schedule.js
+++ teaching/cs455/schedule.js
@@ -27,7 +27,7 @@ let days = {
         name: "Hwk 3 (coding)",
         title: "Data Preparation",
         url: "hwk3.ddl/",
-        due: "10/2/2023",
+        due: "10/4/2023",
       },
     
       // {
