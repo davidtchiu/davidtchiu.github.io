1933f658cc9544ef949b15edd4df02f83a292c0f
David Chiu <david.t.chiu@gmail.com>
2025-01-20 10:33:10 -0800

    161 calendar

--- teaching/cs161/schedule.js
+++ teaching/cs161/schedule.js
@@ -29,54 +29,54 @@ let days = {
         url: "hwk0.me/",
         due: "1/27/2025", // Mon, wk 2
       },
-      {
-        name: "Hwk 1",
-        title: "A Better Circle (Method Calls, Method Writing)",
-        url: "hwk1.circle/",
-        due: "2/5/2025", // Wed, wk 3
-      },
-      {
-        name: "Hwk 2",
-        title: "Alarm Clock (Accumulators, Conditionals)",
-        url: "hwk2.clock/",
-        due: "2/14/2025", // Fri, wk 4
-      },
-      {
-        name: "Hwk 3",
-        title: "Fractions (Object Interaction)",
-        url: "hwk3.fraction/",
-        due: "2/24/2025", // Mon, wk 6
-      },
-      {
-        name: "Hwk 4",
-        title: "Robots (Code Abstraction and Modularity)",
-        url: "hwk4.robot/",
-        due: "3/7/2025", //Fri, wk 7 (has a simple loop)
-      },
-      {
-        name: "Hwk 5",
-        title: "Loops, Loops, Loops!",
-        url: "hwk5.loops/",
-        due: "3/24/2025", // Mon, wk 10 (right after spring break)
-      },
-      {
-        name: "Hwk 6",
-        title: "Credit Card Reader (1D Array)",
-        url: "hwk6.cc/",
-        due: "4/4/2025", // Mon, wk 11 (right after spring break)
-      },
-      {
-        name: "Hwk 7",
-        title: "Images (2D Array)",
-        url: "hwk7.img/",
-        due: "4/14/2025", // Mon, wk 13 (right after spring break)
-      },
-      {
-        name: "Hwk 8",
-        title: "Black Jack",
-        url: "hwkF.blackjack.enums/",
-        due: "5/9/2025", // Fri (exam period)
-      },
+      // {
+      //   name: "Hwk 1",
+      //   title: "A Better Circle (Method Calls, Method Writing)",
+      //   url: "hwk1.circle/",
+      //   due: "2/5/2025", // Wed, wk 3
+      // },
+      // {
+      //   name: "Hwk 2",
+      //   title: "Alarm Clock (Accumulators, Conditionals)",
+      //   url: "hwk2.clock/",
+      //   due: "2/14/2025", // Fri, wk 4
+      // },
+      // {
+      //   name: "Hwk 3",
+      //   title: "Fractions (Object Interaction)",
+      //   url: "hwk3.fraction/",
+      //   due: "2/24/2025", // Mon, wk 6
+      // },
+      // {
+      //   name: "Hwk 4",
+      //   title: "Robots (Code Abstraction and Modularity)",
+      //   url: "hwk4.robot/",
+      //   due: "3/7/2025", //Fri, wk 7 (has a simple loop)
+      // },
+      // {
+      //   name: "Hwk 5",
+      //   title: "Loops, Loops, Loops!",
+      //   url: "hwk5.loops/",
+      //   due: "3/24/2025", // Mon, wk 10 (right after spring break)
+      // },
+      // {
+      //   name: "Hwk 6",
+      //   title: "Credit Card Reader (1D Array)",
+      //   url: "hwk6.cc/",
+      //   due: "4/4/2025", // Mon, wk 11 (right after spring break)
+      // },
+      // {
+      //   name: "Hwk 7",
+      //   title: "Images (2D Array)",
+      //   url: "hwk7.img/",
+      //   due: "4/14/2025", // Mon, wk 13 (right after spring break)
+      // },
+      // {
+      //   name: "Hwk 8",
+      //   title: "Black Jack",
+      //   url: "hwkF.blackjack.enums/",
+      //   due: "5/9/2025", // Fri (exam period)
+      // },
       // {
       //   name: "Final Hwk Prop",
       //   title: "Final Homework Proposal (Optional)",
@@ -98,78 +98,78 @@ let days = {
         url: "lab1.bluej/",
         due: "1/24/2025",
       },
-      {
-        name: "Lab 2",
-        title: "Triangle Class (Variables, Scope, and Lifetime)",
-        url: "lab2.vars/",
-        due: "1/31/2025",
-      },
-      {
-        name: "Lab 3",
-        title: "Stomach Class (Class Writing)",
-        url: "lab3.stomach/",
-        due: "2/7/2025",
-      },
-      {
-        name: "Lab 4",
-        title: "Guessing Game (If-Else)",
-        url: "lab4.guessing/",
-        due: "2/14/2025",
-      },
-      {
-        name: "Lab 5",
-        title: "Circle Drawer (Object Interaction)",
-        url: "lab5.circleDrawer/",
-        due: "2/21/2025",
-      },
-      {
-        name: "Lab 6",
-        title: "Diamonds (Abstraction and Modularity)",
-        url: "lab6.diamonds/",
-        due: "2/28/2025",
-      },
-      {
-        name: "Lab 7",
-        title: "Turtle (Loops Practice)",
-        url: "lab7.loops/",
-        due: "3/7/2025",
-      },
-      {
-        name: "Lab 8",
-        title: "Homework Lab",
-        url: "hwk5.loops/",
-        due: "3/14/2025",
-      },
-      {
-        name: "Lab 9",
-        title: "Combo Guesser (Arrays)",
-        url: "lab9.combo/",
-        due: "3/28/2025",
-      },
-      {
-        name: "Lab 10",
-        title: "The Game of Life (2D Arrays)",
-        url: "lab10.life/",
-        due: "4/4/2025",
-      },
-      {
-        name: "Lab 11",
-        title: "Super Circle Drawer (ArrayLists)",
-        url: "lab11.superCircleDrawer/",
-        due: "3/29/2024",
-      },
-      {
-        name: "Lab 12",
-        title: "Tweet Processor (Scanner, String parsing)",
-        url: "lab12.twitter/",
-        due: "4/18/2025",
-      },
-      {
-        name: "Lab 13",
-        title: "Playing Cards (Constants, Enum)",
-        url: "lab13.cards/",
-        due: "4/19/2024",
-      }
+      // {
+      //   name: "Lab 2",
+      //   title: "Triangle Class (Variables, Scope, and Lifetime)",
+      //   url: "lab2.vars/",
+      //   due: "1/31/2025",
+      // },
+      // {
+      //   name: "Lab 3",
+      //   title: "Stomach Class (Class Writing)",
+      //   url: "lab3.stomach/",
+      //   due: "2/7/2025",
+      // },
+      // {
+      //   name: "Lab 4",
+      //   title: "Guessing Game (If-Else)",
+      //   url: "lab4.guessing/",
+      //   due: "2/14/2025",
+      // },
+      // {
+      //   name: "Lab 5",
+      //   title: "Circle Drawer (Object Interaction)",
+      //   url: "lab5.circleDrawer/",
+      //   due: "2/21/2025",
+      // },
+      // {
+      //   name: "Lab 6",
+      //   title: "Diamonds (Abstraction and Modularity)",
+      //   url: "lab6.diamonds/",
+      //   due: "2/28/2025",
+      // },
+      // {
+      //   name: "Lab 7",
+      //   title: "Turtle (Loops Practice)",
+      //   url: "lab7.loops/",
+      //   due: "3/7/2025",
+      // },
+      // {
+      //   name: "Lab 8",
+      //   title: "Homework Lab",
+      //   url: "hwk5.loops/",
+      //   due: "3/14/2025",
+      // },
+      // {
+      //   name: "Lab 9",
+      //   title: "Combo Guesser (Arrays)",
+      //   url: "lab9.combo/",
+      //   due: "3/28/2025",
+      // },
+      // {
+      //   name: "Lab 10",
+      //   title: "The Game of Life (2D Arrays)",
+      //   url: "lab10.life/",
+      //   due: "4/4/2025",
+      // },
+      // {
+      //   name: "Lab 11",
+      //   title: "Super Circle Drawer (ArrayLists)",
+      //   url: "lab11.superCircleDrawer/",
+      //   due: "3/29/2024",
+      // },
+      // {
+      //   name: "Lab 12",
+      //   title: "Tweet Processor (Scanner, String parsing)",
+      //   url: "lab12.twitter/",
+      //   due: "4/18/2025",
+      // },
+      // {
+      //   name: "Lab 13",
+      //   title: "Playing Cards (Constants, Enum)",
+      //   url: "lab13.cards/",
+      //   due: "4/19/2024",
+      // }
       // {
       //   name: "Lab 10",
       //   title: "Boulders (Files)",
