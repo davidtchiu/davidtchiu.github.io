a6d8a7addaf9363187221a32c58eb1d39c65d0b5
David Chiu <david.t.chiu@gmail.com>
2025-05-26 16:16:44 +0800

    161 calendar thu

--- teaching/cs161.thu/schedule.js
+++ teaching/cs161.thu/schedule.js
@@ -14,75 +14,7 @@ let days = {
     },
   ],
   assignments: {
-    homework: [
-      // {
-      //   name: "Hwk 0",
-      //   title: "All About Me (BlueJ, Code Submission)",
-      //   url: "hwk0.me/",
-      //   due: "1/27/2025", // Mon, wk 2
-      // },
-      // {
-      //   name: "Hwk 1",
-      //   title: "A Better Circle (Method Calls, Method Writing)",
-      //   url: "hwk1.circle/",
-      //   due: "6/2/2025", // Wed, wk 3
-      // },
-      // {
-      //   name: "Hwk 2",
-      //   title: "Alarm Clock (Accumulators, Conditionals)",
-      //   url: "hwk2.clock/",
-      //   due: "2/14/2025", // Fri, wk 4
-      // },
-      // {
-      //   name: "Hwk 3",
-      //   title: "Fractions (Object Interaction)",
-      //   url: "hwk3.fraction/",
-      //   due: "2/26/2025", // Mon, wk 6
-      // },
-      // {
-      //   name: "Hwk 4",
-      //   title: "Robots (Code Abstraction and Modularity)",
-      //   url: "hwk4.robot/",
-      //   due: "3/7/2025", //Fri, wk 7 (has a simple loop)
-      // },
-      // {
-      //   name: "Hwk 5",
-      //   title: "Loops, Loops, Loops!",
-      //   url: "hwk5.loops/",
-      //   due: "3/25/2025", // Tues, wk 10 (right after spring break)
-      // },
-      // {
-      //   name: "Hwk 6",
-      //   title: "Credit Card Reader (1D Array)",
-      //   url: "hwk6.cc/",
-      //   due: "4/4/2025", // Mon, wk 11 (right after spring break)
-      // },
-      // {
-      //   name: "Hwk 7",
-      //   title: "Images (2D Array)",
-      //   url: "hwk7.img/",
-      //   due: "4/14/2025", // Mon, wk 13 (right after spring break)
-      // },
-      // {
-      //   name: "Hwk 8",
-      //   title: "Black Jack",
-      //   url: "hwkF.blackjack.enums/",
-      //   due: "5/9/2025", // Fri (exam period)
-      // },
-      // {
-      //   name: "Final Hwk Prop",
-      //   title: "Final Homework Proposal (Optional)",
-      //   url: "hwkF.prep/",
-      //   due: "4/11/2025",  // Fri, wk 12
-      // },
-      // // // {
-      // // //   name: "Final Hwk (alternate)",
-      // // //   title: "Pooled Testing",
-      // // //   url: "hwkF.pooled/",
-      // // //   due: "5/4/2021",
-      // // // },
-    ],
-    /////////////////////////////////// labs /////////////////////////////////////////
+      /////////////////////////////////// labs /////////////////////////////////////////
     labs: [
       {
         name: "Lab 1",
@@ -132,7 +64,7 @@ let days = {
     // wk 1
     "What are algorithms?",
     "Hardware, Languages; Lab 1",
-    "Basic class structure",
+    "Java class structure",
     "Variables: data types, lifetime, and scope; Quiz",
     "<b>No class: Dragon Boat Festival</b>",
     // wk 2
