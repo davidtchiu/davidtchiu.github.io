e4868b9cd2dd1f35eb3d102484b8f56da785da82
David Chiu <david.t.chiu@gmail.com>
2026-04-15 20:17:20 -0700

    testing notes section of cal

--- teaching/cs161/schedule.js
+++ teaching/cs161/schedule.js
@@ -284,6 +284,7 @@ let days = {
     "Scanner: File handling",
       "Notes: <a href='https://drive.google.com/file/d/1uISc-ZRQSVfUojvLCr7ftMp3a0KFs0zg/view?usp=drive_link'>11. Scanner</a>" + "<br/>" +
       "Code: <a href='https://drive.google.com/file/d/187sn3agxiSCDslKSoP4EzpHft8NWDVMF/view?usp=drive_link'>ChatBot</a> (starter)",
+      "Code: <a href='https://drive.google.com/file/d/15cV3MUWcoHBPKZb_uSkicfsZq060nFi6/view?usp=drive_link'>ChatBot</a> (finished)",
     // wk 14
     "Static scope, enums",
     "Lab 13: Cards (Enum type)",
