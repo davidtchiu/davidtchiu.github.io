f4412b1fa81cdf855df4c657af8ab8e2e7dcb099
David Chiu <david.t.chiu@gmail.com>
2026-07-14 10:53:12 -0700

    testing notes section of cal

--- teaching/cs498/schedule.js
+++ teaching/cs498/schedule.js
@@ -15,60 +15,63 @@ let days = {
     // wk 1
     "Exploration: Capstone sorting exercise", // Give students a list of possible project ideas: some too small, some too large, some vague, some feasible. Have them classify each one as “too small,” “reasonable,” “too ambitious,” or “unclear.”
     "Exploration: Project repair lab", // Give students project ideas, and get then to repair them so they are more in scope.
-      "Mon: <a href='https://docs.google.com/document/d/1e_NsdxjyYxz_24HmNAQc9G1LL7XH3xYcJH2AMQa_F1Y/edit?usp=drive_link'>Idea Classification</a>'" + "<br/>" + 
-      "Fri: <a href='https://docs.google.com/document/d/19oZe04G3wZL7xSLWYL6kKj3xkMyJTAfPALX6S3lA92o/edit?usp=drive_link'>Repair Lab</a>'"
+      "Mon: <a href='https://docs.google.com/document/d/1e_NsdxjyYxz_24HmNAQc9G1LL7XH3xYcJH2AMQa_F1Y/edit?usp=drive_link'>1a. Idea Classification</a>'" + "<br/>" + 
+      "Fri: <a href='https://docs.google.com/document/d/19oZe04G3wZL7xSLWYL6kKj3xkMyJTAfPALX6S3lA92o/edit?usp=drive_link'>1b. Repair Lab</a>'"
       ,
     // wk 2
     "<strong>MLK Day<br/>(no class)</strong>",
     "Exploration: Idea generation", // Come up with at least 3 project directions
-      "Fri: <a href='https://docs.google.com/document/d/1wRyQCQUXBkwbegbVCaENkez6tRVuKPwPpB865eEz-vc/edit?usp=drive_link'>Idea Radar</a>"
+      "Fri: <a href='https://docs.google.com/document/d/1wRyQCQUXBkwbegbVCaENkez6tRVuKPwPpB865eEz-vc/edit?usp=drive_link'>2. Idea Radar</a>"
     ,
     // wk 3
     "Project def'n: Alternatives comparison", // Students compare 3 candidate projects using criteria: interest, feasibility, novelty, technical depth, data availability, evaluation, and risk. Each student/team explains what they chose and what they rejected.
     "Project def'n: Project definition and personas", 
-      "Mon: <a href='https://docs.google.com/document/d/1Vh7G7ENYOxLLMkSQplFwhA4nQrlhos8NAXd29UebsSo/edit?usp=drive_link'>Project Statement</a>" + "<br/>" +
-      "Fri: <a href='https://docs.google.com/document/d/1NKW0GdXv4KrLQ_w5l8ZO2hqQSMaKUtkssyVez2e7eOc/edit?usp=drive_link'>Personas</a>"
+      "Mon: <a href='https://docs.google.com/document/d/1Vh7G7ENYOxLLMkSQplFwhA4nQrlhos8NAXd29UebsSo/edit?usp=drive_link'>3a. Project Statement</a>" + "<br/>" +
+      "Fri: <a href='https://docs.google.com/document/d/1NKW0GdXv4KrLQ_w5l8ZO2hqQSMaKUtkssyVez2e7eOc/edit?usp=drive_link'>3b. Personas</a>"
     ,
     // wk 4
     "Problem statement", 
     "Problem statement (cont.)",
-      "Mon: <a href='https://docs.google.com/document/d/1HNnkLD7bKSad4XP8tS_e_Wug2Gj61O-2msVDAJ8gd3M/edit?usp=drive_link'>Snapshot</a>" + "<br/>" +
-      "Fri: <a href='https://docs.google.com/document/d/1ivP8vnTAOsufktrSjXKBtSkc1Bj0of2730jyR4cPagA/edit?usp=drive_link'>Research Questions</a>"
+      "Mon: <a href='https://docs.google.com/document/d/1HNnkLD7bKSad4XP8tS_e_Wug2Gj61O-2msVDAJ8gd3M/edit?usp=drive_link'>4a. Snapshot</a>" + "<br/>" +
+      "Fri: <a href='https://docs.google.com/document/d/1ivP8vnTAOsufktrSjXKBtSkc1Bj0of2730jyR4cPagA/edit?usp=drive_link'>4b. Research Questions</a>"
     ,
     // wk 5
     "AI tool finding", // 
     "AI tool testing", // 
-      "Mon: <a href='https://docs.google.com/document/d/1ZBjDU0eWnfvt8yiYDKkwNPkeY-xBDgIy5Rfzs2zrPr4/edit?usp=drive_link'>AI Tasks</a>" + "<br/>" +
-      "Fri: <a href='https://docs.google.com/document/d/1X3A0q_rQrI-QdFnkCrSLJUmSGAPuErvdf1slQq-ILdE/edit?usp=drive_link'>AI Tools Experiment</a>"
+      "Mon: <a href='https://docs.google.com/document/d/1ZBjDU0eWnfvt8yiYDKkwNPkeY-xBDgIy5Rfzs2zrPr4/edit?usp=drive_link'>5a. AI Tasks</a>" + "<br/>" +
+      "Fri: <a href='https://docs.google.com/document/d/1X3A0q_rQrI-QdFnkCrSLJUmSGAPuErvdf1slQq-ILdE/edit?usp=drive_link'>5b. AI Tools Experiment</a>"
       ,
     // wk 6
     "Background: Prior work hunt", // Students find examples of similar apps, papers, GitHub repos, datasets, tools, or APIs. They classify each as inspiration, competitor, dependency, or warning sign.
     "Background: Choices", // tech, resource, and data discussions
-      "Mon: <a href='https://docs.google.com/document/d/1RmAVoe_YKqwn0U1gf8nysLG9DvZNuA9nNSEq81F5OyU/edit?usp=drive_link'>Related Work</a>" + "<br/>" +
-      "Fri: <a href='https://docs.google.com/document/d/1lI7D-BhGg9wyO2NhBmHVRwo_yyRklQWxdQJlSAVW2qU/edit?usp=drive_link'>Needs and Choices</a>"
+      "Mon: <a href='https://docs.google.com/document/d/1RmAVoe_YKqwn0U1gf8nysLG9DvZNuA9nNSEq81F5OyU/edit?usp=drive_link'>6a. Related Work</a>" + "<br/>" +
+      "Fri: <a href='https://docs.google.com/document/d/1lI7D-BhGg9wyO2NhBmHVRwo_yyRklQWxdQJlSAVW2qU/edit?usp=drive_link'>6b. Needs and Choices</a>"
       ,
     // wk 7
     "Risks: Ethics study", // 
     "Risks: Pre-mortem and mitigation", // premortem and mitigation
-    "",
-    // wk 7
-    "Feasibility", 
-    "Feasibility: MVP and project scope", 
-    "",
-    // wk 8
+      "Mon: <a href='https://docs.google.com/document/d/12RhqMoLPqzR4X5Y4IITnt39ydfj9KlgAg4JUHKsyIDI/edit?usp=drive_link'>7a. Ethics</a>" + "<br/>" +
+      "Fri: <a href='https://docs.google.com/document/d/12ZOkl3fmQb-FNXbrfMw1aUfDIU6fxr9YIqh2XEVEA-Y/edit?usp=drive_link'>7b. Stress Test</a>"
+      ,
+    // wk 8 (fall break)
     "<strong>Fall Break</strong>",
+    "Feasibility", 
+      "Fri: <a href='https://docs.google.com/document/d/1MsObp789Q_eMbSkhBdDzjWNn3W6BnP3uhjtwk4Po0gw/edit?usp=drive_link'>8a. Feasibility</a>"
+        //+ "<br/>" +
+      // "Fri: <a href='https://docs.google.com/document/d/1U6TYbQ51M88OZUwiNRlA_Ixdgxqe9xeZ3Z6vcHEZLI0/edit?usp=drive_link'>8b. Scope</a>"
+      ,
+    // wk 9
     "Use-case: User stories",
-    "",
-    // wk 9: 
-    "Use-case: User stories", 
-    "Use-case: First storyboard", 
-    // "Input/output audit", // For every major component, students define inputs, outputs, data formats, and failure cases.",
-    // "Check whether the components are real enough to implement", //. This is where vague “AI module” boxes get challenged.",
-    "",
-    // wk 10
+    "Use-case: Storyboard",
+      "Mon: <a href='https://docs.google.com/document/d/1g4u2-Vn784nJ-8MAL4jPRLTrZXvMpy-vwgpAPOslxw4/edit?usp=drive_link'>9a. User stories</a>" + "<br/>" +
+      "Fri: <a href='https://docs.google.com/document/d/1XDMFTchr4XY0kkRfp1UiL5Z0s0FNkdSaG0XW_bdSg0Q/edit?usp=drive_link'>9b. Storyboard 1</a>",
+    ,
+      // wk 10
     "Use-case: First storyboard", 
     "Architecture: ", 
-    "",
+      "Mon: <a href='https://docs.google.com/document/d/1g4u2-Vn784nJ-8MAL4jPRLTrZXvMpy-vwgpAPOslxw4/edit?usp=drive_link'>10a. Storyboard 2</a>" + "<br/>" +
+      "Fri: <a href='https://docs.google.com/document/d/1hZoI3ZjRxfeks_O1fdCvcy55m_f1nqbZjKGTK00vAaI/edit?usp=drive_link'>10b. Architecture</a>",
+    ,
     // wk 11
     "Challenges and tradeoffs", 
     "Cont'd",
