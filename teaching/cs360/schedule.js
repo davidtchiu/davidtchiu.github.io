222091c698a502d585f0e91c034a101247f6fe5c
David Chiu <david.t.chiu@gmail.com>
2026-09-21 14:45:22 -0700

    changes to lab 1

--- teaching/cs360/schedule.js
+++ teaching/cs360/schedule.js
@@ -139,7 +139,9 @@ let days = {
     "SQL: schema definition",
     "SQL: writing basic queries, joins",
     "SQL: outer joins, renaming, temporary tables",
-      "",
+      "Notes: <a href='https://drive.google.com/open?id=1zntL-g7yGuUg_vNiHy-7lTcZxLSb3TfH&usp=drive_fs'>3a. SQL I</a>" + "<br/>" +
+      "Hand out: <a href='https://drive.google.com/open?id=1J_wvCXxMoXLIolvBnkVEm72nMUZDIeSI'>SQL Cheatsheet</a>" + "<br/>"
+      ,
     // wk 5
     "SQL: subqueries",
     "SQL: grouping and aggregation",
